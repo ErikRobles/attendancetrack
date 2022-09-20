5720491f3d30856e9af652680aed4a12584a9fbb
ErikRobles <erikjames69@hotmail.com>
2022-09-20 08:34:20 -0500

    adjusted db data

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,10 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{-- {{ __('Dashboard') }} --}}
+            <x-slot name="logo">
+                <img src="{{ public_path('backend/img/greylogo.png') }}"  style="width: 150px;" alt="">
+            </x-slot>
         </h2>
     </x-slot>
 
